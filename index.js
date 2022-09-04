2fff1cae2c9366b8ac7121098927e55dddd95624
WardKhaddour <wardkhaddot@gmail.com>
2022-09-04 03:10:12 +0300

    convert to server side rendering

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5500',
+    origin: 'https://tic-tac-toe22.herokuapp.com/',
   },
 });
 
